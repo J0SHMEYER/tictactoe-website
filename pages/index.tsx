4a63260117ba5423e6a7b107f5060faf79ee8e5f
J0SHMEYER <100731166+J0SHMEYER@users.noreply.github.com>
2022-09-07 16:30:05 +1200

    host to vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Board from '../containers/Board'
+import Board from '../components/Board'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
